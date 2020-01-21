4a722210c7e372d7a20e2fbc494aa58a7ebed3d8
vn <kronoslang@fastmail.com>
2020-01-21 21:51:36 +0200

    port back to darwin

--- doall.sh
+++ doall.sh
@@ -7,5 +7,10 @@ echo "Running benchmarks, prefix ID \"${PREFIX}_\""
 for patch in *.pd
 do
 echo "[$patch]"
-script -qc "bash ./bench.sh $patch" "${PREFIX}_${patch%.pd}.csv"
+if [[ $OSTYPE == "darwin"* ]];
+then
+	script -q "${PREFIX}_${patch%.pd}.csv" bash ./bench.sh $patch
+else
+	script -qc "bash ./bench.sh $patch" "${PREFIX}_${patch%.pd}.csv"
+fi
 done
